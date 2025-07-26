5592d1c068a9337f63cd186cb7ba33c8941dd4a9
VISHMITHAWDL <vishmithahiru68@gmail.com>
2025-07-26 13:38:06 +0530

    feat: add initial project structure with Maven setup, application properties, and basic Spring Boot application

---
+++ server/tendora_server/src/test/java/com/tendora/tendora_server/TendoraServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tendora.tendora_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TendoraServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
